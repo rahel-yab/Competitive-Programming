79233a245915a816ca4f6a5bf3e1b0f59f809faf
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-03-26 19:40:47 +0300

    Created 'The Meeting Place Cannot Be Changed' by A2SV Hub

---
+++ 26-Mar-2025/The Meeting Place Cannot Be Changed 319075.py
@@ -0,0 +1,31 @@
+# Problem: The Meeting Place Cannot Be Changed - https://codeforces.com/problemset/problem/780/B
+
+n = int(input())
+x = list(map(int, input().split()))
+v = list(map(int, input().split()))
+
+
+def yigerem(time):
+    max_left = float('-inf')
+    min_right = float('inf')
+    for xi, vi in zip(x,v):
+        displacement = vi * time
+        left = xi - displacement
+        right = xi + displacement
+        max_left = max(max_left,left)
+        min_right = min(min_right, right)
+    
+
+    return max_left <= min_right
+
+
+low = 0
+high = max(x) - min(x)
+precision = 10 ** -6
+while low <= high:
+    mid = (low + high) / 2
+    if yigerem(mid):
+        high = mid - precision
+    else:
+        low = mid + precision
+print(low)
