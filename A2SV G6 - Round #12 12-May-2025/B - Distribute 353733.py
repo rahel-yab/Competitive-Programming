d23f78de5634c981738487a4163db1adc21477a0
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-05-12 17:28:08 +0300

    Created 'B - Distribute' by A2SV Hub

---
+++ A2SV G6 - Round #12 12-May-2025/B - Distribute 353733.py
@@ -0,0 +1,22 @@
+# Problem: B - Distribute - https://codeforces.com/gym/606404/problem/B
+
+n = int(input())
+nums = list(map(int, input().split()))
+new = sorted(nums)
+mapp = {}
+for i in range(n):
+    if nums[i] in mapp:
+        mapp[nums[i]].append(i)
+    else:
+        mapp[nums[i]] = [i]
+
+left = 0
+right = n - 1
+pair = []
+
+while left < right:
+    pair.append((new[left], new[right]))
+    left += 1
+    right -= 1
+for u, v in pair:
+    print(mapp[u].pop() + 1, mapp[v].pop() + 1)
