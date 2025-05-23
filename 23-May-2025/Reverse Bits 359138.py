d04ccf29d54af9d63f01f99c04609eb77ae4bf21
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-05-23 10:59:51 +0300

    Created 'Reverse Bits' by A2SV Hub

---
+++ 23-May-2025/Reverse Bits 359138.py
@@ -0,0 +1,10 @@
+# Problem: Reverse Bits - https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for _ in range(32):
+            bit = n & 1
+            res = (res << 1) | bit
+            n >>= 1
+        return res
