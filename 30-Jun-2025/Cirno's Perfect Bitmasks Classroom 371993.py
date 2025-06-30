fab7b8324a2cfa17f5be5582e30fd6c5fdfcf7ad
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-06-30 13:41:51 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 30-Jun-2025/Cirno's Perfect Bitmasks Classroom 371993.py
@@ -0,0 +1,10 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    # print( 1000000 & -1000000)
+    y = x & -x
+    while x ^ y == 0 or x & y == 0:
+        y += 1
+    print(y)
