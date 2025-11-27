55e0e9dad8ab4f5f435fa1501cb58331c777750c
Rahel Yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-11-27 22:18:57 +0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (78.05%) - LeetSync

--- 979-di-string-match/di-string-match.py
+++ 979-di-string-match/di-string-match.py
@@ -1,7 +1,6 @@
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
         n = len(s)
-        nums = [i for i in range(n+1)]
 
         left = 0
         right = n
@@ -9,20 +8,15 @@ class Solution:
         i = 0
         while left <= right and i < n:
             if s[i] == 'I':
-                ans.append(nums[left])
+                ans.append(left)
                 left += 1
             else:
-                ans.append(nums[right])
+                ans.append(right)
                 right -= 1
             
             i+=1
 
-        sett = set(ans)
-        for num in range(n+1):
-            if num not in sett:
-                ans.append(num)
-                break
-
+        ans.append(left)
         return ans
             
 
