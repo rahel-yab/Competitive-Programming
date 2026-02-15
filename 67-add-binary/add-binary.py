2eee047aca001327f52bb6a91ba33d53a14e39f2
Rahel Yabebal <135514994+rahel-yab@users.noreply.github.com>
2026-02-15 22:58:08 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (37.00%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        ans = []
+        c = 0
+        i, j = len(a) - 1, len(b) - 1
+
+        while i >= 0 or j >= 0 or c:
+            val_a = int(a[i]) if i >= 0 else 0
+            val_b = int(b[j]) if j >= 0 else 0
+            
+            total = val_a + val_b + c
+            ans.append(str(total % 2))
+            c = total // 2         
+            
+            i -= 1
+            j -= 1
+            
+        return ''.join(ans[::-1])
