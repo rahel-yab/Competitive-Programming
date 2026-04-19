dcb326c998a293ab2f7d25d1d31cf3591abe9f4c
Rahel Yabebal <135514994+rahel-yab@users.noreply.github.com>
2026-04-19 22:44:05 +0300

    Time: 573 ms (5.14%) | Memory: 36.1 MB (20.89%) - LeetSync

---
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+
+        def binary_search(target):
+            low = 0
+            high = len(nums2)-1
+
+            while low <= high:
+                mid = (low+high) // 2
+                if nums2[mid] >= target:
+                    low = mid + 1
+                else:
+                    high = mid -1
+
+            return high
+
+        for i , num in enumerate(nums1):
+            ind = binary_search( num)
+            ans = max(ans , ind-i)
+
+        return ans 
